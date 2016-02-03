255ad59ef7fbaf3dc01feaeccd154fe5e32f1929
Steve Good <steve@stevegood.org>
2016-02-03 16:11:14 -0600

    Blatantly copied the ratpack standalone app from the ratpack repos

---
+++ ratpack/standalone-script/ratpack.groovy
@@ -0,0 +1,24 @@
+@GrabResolver(name="OJO", root="https://oss.jfrog.org/artifactory/repo")
+@Grab("io.ratpack:ratpack-groovy:1.1.1")
+import static ratpack.groovy.Groovy.ratpack
+import static ratpack.groovy.Groovy.groovyMarkupTemplate
+import ratpack.groovy.template.MarkupTemplateModule
+
+// You can change anything in the ratpack {} closure without needing to restart
+
+ratpack {
+    bindings {
+        module MarkupTemplateModule
+    }
+    handlers {
+        get {
+            response.send "This is the app root (also try: /date and /some.txt)"
+        }
+
+        get("date") {
+            render groovyMarkupTemplate("date.gtpl")
+        }
+
+        files { dir "public" }
+    }
+}
